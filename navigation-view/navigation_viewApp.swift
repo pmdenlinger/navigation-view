d9e42c90c8a6ebad7fb62ea615c348eb54bc5e82
Paul Denlinger <pmdenlinger@icloud.com>
2021-06-21 17:31:27 -0700

    Initial Commit

---
+++ navigation-view/navigation_viewApp.swift
@@ -0,0 +1,17 @@
+//
+//  navigation_viewApp.swift
+//  navigation-view
+//
+//  Created by user on 6/21/21.
+//
+
+import SwiftUI
+
+@main
+struct navigation_viewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
